12a4f505257fe3c8b48355f37896c7a2611e81e2
egublog <egukei0729@gmail.com>
2024-09-03 00:56:07 +0900

    modify: NavbarとSidebar

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,4 +1,6 @@
 import React, { ReactNode } from 'react'
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 
 /**
  * Home画面のメインレイアウト
@@ -6,10 +8,10 @@ import React, { ReactNode } from 'react'
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className='relative'>
-      Navbar
+      <Navbar />
 
       <div className='flex'>
-        Sidebar
+        <Sidebar />
 
         <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
           <div className='w-full'>
