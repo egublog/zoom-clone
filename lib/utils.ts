0369264874b0bcc229f48c2484e2f46d95181bc6
egublog <egukei0729@gmail.com>
2024-09-06 00:32:13 +0900

    add: cn()のコメント

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * クラス名を結合し、最適化されたクラス名の文字列を返します。
+ * @param inputs - 結合するクラス名のリスト
+ * @returns 最適化されたクラス名の文字列
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
