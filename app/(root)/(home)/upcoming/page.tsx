b00d4c643d1d6625b5b9ec7bd410830baaf2f86e
egublog <egukei0729@gmail.com>
2024-09-13 04:31:40 +0900

    add: Upcomingコンポーネント

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+/**
+ * Upcomingコンポーネント
+ *
+ * このコンポーネントは、"Upcoming"セクションを表示します。
+ */
 const Upcoming = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
