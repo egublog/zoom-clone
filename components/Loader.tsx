a70d261a0b89cc01d681b9a0e8973d54477b9bb0
egublog <egukei0729@gmail.com>
2024-10-03 07:59:54 +0900

    add: Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 
+/**
+ * Loaderコンポーネント
+ *
+ * このコンポーネントは、ローディング中のインジケーターを表示します。
+ * 画面全体に中央揃えでローディングアイコンを表示します。
+ */
 const Loader = () => {
   return (
     <div className="flex-center h-screen w-full">
