a4a0ba067d4d15dc13e697e6936a18f691077a6f
egublog <egukei0729@gmail.com>
2024-10-04 07:35:13 +0900

    add: Meetingのクラスコメント

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+/**
+ * Meetingコンポーネント
+ *
+ * このコンポーネントは、特定のIDを持つ会議室を表示します。
+ * URLパラメータから会議IDを取得し、表示します。
+ *
+ * @param params - URLから取得したパラメータ
+ * - id: 表示する会議室のID
+ */
 const Meeting = ({ params }: { params: { id: string } }) => {
   return (
     <div>Meeting Room: #{params.id}</div>
