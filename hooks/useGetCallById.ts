ed551e63f84bf1aefe3be44f87fcb82504b9d062
egublog <egukei0729@gmail.com>
2024-10-12 23:41:18 +0900

    add: useGetCallByIdフックのクラスコメント

--- hooks/useGetCallById.ts
+++ hooks/useGetCallById.ts
@@ -1,6 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 
+/**
+ * useGetCallByIdフック
+ *
+ * このフックは、指定されたIDに基づいてビデオ通話を取得します。
+ * 通話がロードされるまでの間、ローディング状態を管理します。
+ *
+ * @param id - 取得する通話のIDまたはIDの配列
+ * @returns {Object} - 取得した通話オブジェクトとローディング状態を含むオブジェクト
+ * - call: 取得した通話オブジェクト
+ * - isCallLoading: 通話がロード中かどうかを示すフラグ
+ */
 export const useGetCallById = (id: string | string[]) => {
 	const [call, setCall] = useState<Call>();
 	const [isCallLoading, setIsCallLoading] = useState(true);
